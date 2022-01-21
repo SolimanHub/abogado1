b15eebb5db50dc2ec123e436807b2e50c0175eee
solimanlab <dse_x@hotmail.es>
2022-01-21 02:13:13 -0600

    avance en el div que muestra los servicios que se ofrecen

---
+++ js/main.js
@@ -0,0 +1,41 @@
+$(document).ready(function() {
+  //boton volver en detalles del area
+  $("#btn_volver").click(function(){
+    document.getElementById('overlay').style.display = 'none';
+  });
+  //================
+  var name_click = "";
+  var name_click_temp;
+
+  $(".container_serv").click(function(){
+    name = $(this).attr('id');
+    document.getElementById('sd').style.display = 'block';
+  });
+});//fin ready
+
+
+function click_in_service() {
+  var name = "";
+  var booldisplayserv = 0;
+  var nameTemp;
+  $('.container_serv').click(function(){
+    if (name != ""){
+      document.getElementById(name).style.display = 'none';
+      nameTemp = name;
+    }
+    console.log($(this).attr('id'));
+    name = $(this).attr('id');
+    name = name + ".3";
+    console.log(name);
+    //$(name).css("display: block");
+    if (nameTemp == name && booldisplayserv == 1){
+      booldisplayserv = 0;
+      document.getElementById(name).style.display = 'none';
+    }
+    else{
+      document.getElementById(name).style.display = 'flex';
+      booldisplayserv = 1;
+    }
+  });
+
+}
